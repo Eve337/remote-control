0a917f505f5f10b8693ae19b8dd5cee2e2c29d07
eve337 <vesunchik1945@gmail.com>
2022-06-26 16:26:13 +0300

    Add: added draw operations

--- src/robotOperations/index.ts
+++ src/robotOperations/index.ts
@@ -28,3 +28,53 @@ export const moveRight = (inputX: string) => {
 export const mousePosition = () => {
   return robot.getMousePos();
 }
+
+export const drawCircle = (radius: string) => {
+  const { x, y } = robot.getMousePos();
+  const radiusConverted = Number(radius);
+
+  robot.moveMouse(
+    x + (radiusConverted * Math.cos(0)), y + (radiusConverted * Math.sin(0))
+  );
+  robot.mouseToggle('down');
+
+  for (let i = 0; i <= Math.PI * 2; i += 0.01) {
+    
+    const newX = x + (radiusConverted * Math.cos(i));
+    const newY = y + (radiusConverted * Math.sin(i));
+    
+    robot.moveMouseSmooth(newX, newY);
+}
+
+  robot.mouseToggle('up');
+
+}
+
+export const drawSquare = (lengthOfEdge: string) => {
+  const { x, y } = robot.getMousePos();
+  const convertedLength = Number(lengthOfEdge);
+
+  robot.mouseToggle('down');
+
+  robot.moveMouseSmooth(x, y + convertedLength);
+  robot.moveMouseSmooth(x - convertedLength, y + convertedLength);
+  robot.moveMouseSmooth(x - convertedLength, y);
+  robot.moveMouseSmooth(x, y);
+
+  robot.mouseToggle('up');
+}
+
+export const drawRectangle = (height: string, width: string) => {
+  const { x, y } = robot.getMousePos();
+  const convertedHeight = Number(height);
+  const convertedWidth = Number(width);
+
+  robot.mouseToggle('down');
+
+  robot.moveMouseSmooth(x, y + convertedWidth);
+  robot.moveMouseSmooth(x - convertedHeight, y + convertedWidth);
+  robot.moveMouseSmooth(x - convertedHeight, y);
+  robot.moveMouseSmooth(x, y);
+
+  robot.mouseToggle('up');
+}
